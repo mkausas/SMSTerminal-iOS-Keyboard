1b76f047cfa47fb3c864555ea5e4322db68d6e34
mkausas <mkausas@gmail.com>
2016-02-21 00:20:57 -0500

    Grabbing static commands and printing them as well.

--- TerminalKeyboard/TerminalViewController.swift
+++ TerminalKeyboard/TerminalViewController.swift
@@ -10,36 +10,80 @@ import UIKit
 
 class TerminalViewController: UIInputViewController {
     
+    
     @IBOutlet weak var collectionView: UICollectionView!
     var insets = UIEdgeInsets(top: 3, left: 3, bottom: 3, right: 3)
     
+    @IBOutlet weak var nextKeyboardButton: UIButton!
+    
+    var commands: [String: [String]] =
+    [
+        "ls": ["-l", "-a"],
+        "pwd": [],
+        "cd": [".."]
+    ]
+    
+    var commandKeys = [String]()
+//    var currentCommands = [String]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+                
         collectionView.dataSource = self
         collectionView.delegate = self
         
         collectionView.registerNib(CommandCell.nib(), forCellWithReuseIdentifier: commandCellIdentifier)
+        
+        nextKeyboardButton.addTarget(self, action: "advanceToNextInputMode", forControlEvents: .TouchUpInside)
+        
+        print("did happen")
+        
+        // grab key list
+        for key in commands.keys {
+            commandKeys.append(key)
+            print("Key = \(key)")
+        }
+        
+        print(commandKeys)
     }
+    
+    
+    
 }
 
 extension TerminalViewController: UICollectionViewDataSource {
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        
-        return 10
+        return commandKeys.count
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(commandCellIdentifier, forIndexPath: indexPath) as! CommandCell
         
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(commandCellIdentifier, forIndexPath: indexPath)
+        cell.commandButton.setTitle(commandKeys[indexPath.row], forState: .Normal)
+        cell.commandButton.addTarget(self, action: "pressedKey:", forControlEvents: .TouchDown)
         
         return cell
     }
+    
+    @IBAction func pressedKey(sender: AnyObject) {
+//        print("pressed button!")
+        let button = sender as! UIButton
+        let title = button.titleLabel?.text
+        print("pressed \(title!)")
+        
+        self.textDocumentProxy.insertText("\(title!) ")
+    }
+    
 }
 
 extension TerminalViewController: UICollectionViewDelegate {
     
+    internal func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        print("pressed collectioncell")
+        
+    }
+
 }
 
 

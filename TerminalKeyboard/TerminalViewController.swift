4ba543341d9edea6999e8d0a92079a3596b9dabb
mkausas <mkausas@gmail.com>
2016-02-21 08:10:44 -0500

    Added data persistance between exiting the keyboard and coming back

--- TerminalKeyboard/TerminalViewController.swift
+++ TerminalKeyboard/TerminalViewController.swift
@@ -18,26 +18,11 @@ class TerminalViewController: UIInputViewController {
     
     @IBOutlet weak var recentCommandButton: UIButton!
     
-    var fullArgs: [String] = ["-a", "-b", "-c", "-d", "-e", "-f", "-g", "-h", "-i", "-j", "-k", "-l", "-m", "-n", "-o", "-p", "-q", "-r", "-s", "-t", "-u", "-v", "-w", "-x", "-y", "-z"]
+    var fullArgs: [String] = ["-a", "-b", "-c", "-d", "-e", "-f", "-g", "-h", "-i", "-j", "-k", "-l", "-m", "-n", "-o", "-p", "-q", "-r", "-s", "-t", "-u", "-v", "-w", "-x", "-y", "-z", "-A", "-B", "-C", "-D", "-E", "-F", "-G", "-H", "-I", "-J", "-K", "-L", "-M", "-N", "-O", "-P", "-Q", "-R", "-S", "-T", "-U", "-V", "-W", "-X", "-Y", "-Z"]
     
     var commands: [String: [String]] =
     [
-        "ls": ["-l", "-a"],
-        "pwd": [],
-        "cd": [".."],
-        "git": ["config", "init", "clone", "add", "status", "diff", "commit", "reset", "rm", "mv", "branch",  "checkout" ,"merge" ,"mergetool" ,"log" ,"stash" ,"tag", "fetch", "pull", "push", "remote", "submodule", "show", "log", "diff", "shortlog", "describe", "apply", "cherry-pick", "diff", "rebase", "revert", "bisect", "blame", "grep", "am", "apply", "format-patch", "send-email", "request-pull", "svn", "fast-import", "clean", "gc", "fsck", "reflog", "filter-branch", "instaweb", "archive", "bundle", "daemon", "update-server-info", "cat-file", "commit-tree", "count-objects", "diff-index", "for-each-ref", "hash-object", "ls-files", "merge-base", "read-tree", "rev-list", "rev-parse", "show-ref", "symbolic-ref", "update-index", "update-ref", "verify-pack", "write-tree"],
-        "mkdir": [],
-        "rmdir": ["-r"],
-        "cp": [],
-        "mv": [],
-        "rm": ["-r"],
-        "|": [],
-        "<": [],
-        ">": [],
-        "&": [],
-        "man": [],
-        "cat": [],
-        "make": []
+ "|": [], "<": [], ">": [], "&": [], "admin": [], "alias": [], "ar": [], "asa": [], "at": [], "awk": [], "basename": [], "batch": [], "bc": [], "bg": [], "cc": [], "cal": [], "cat": [], "cd": [], "cflow": [], "chgrp": [], "chmod": [], "chown": [], "cksum": [], "cmp": [], "comm": [], "command": [], "compress": [], "cp": [], "crontab": [], "csplit": [], "ctags": [], "cut": [], "cxref": [], "date": [], "dd": [], "delta": [], "df": [], "diff": [], "dirname": [], "du": [], "echo": [], "ed": [], "env": [], "ex": [], "expand": [], "expr": [], "false": [], "fc": [], "fg": [], "file": [], "find": [], "fold": [], "fort77": [], "fuser": [], "gencat": [], "get": [], "getconf": [], "getopts": [], "grep": [], "git": ["config", "init", "clone", "add", "status", "diff", "commit", "reset", "rm", "mv", "branch",  "checkout" ,"merge" ,"mergetool" ,"log" ,"stash" ,"tag", "fetch", "pull", "push", "remote", "submodule", "show", "log", "diff", "shortlog", "describe", "apply", "cherry-pick", "diff", "rebase", "revert", "bisect", "blame", "grep", "am", "apply", "format-patch", "send-email", "request-pull", "svn", "fast-import", "clean", "gc", "fsck", "reflog", "filter-branch", "instaweb", "archive", "bundle", "daemon", "update-server-info", "cat-file", "commit-tree", "count-objects", "diff-index", "for-each-ref", "hash-object", "ls-files", "merge-base", "read-tree", "rev-list", "rev-parse", "show-ref", "symbolic-ref", "update-index", "update-ref", "verify-pack", "write-tree"], "hash": [], "head": [], "iconv": [], "id": [], "ipcrm": [], "ipcs": [], "jobs": [], "join": [], "kill": [], "lex": [], "link": [], "ln": [], "locale": [], "localedef": [], "logger": [], "logname": [], "lp": [], "ls": [], "m4": [], "mailx": [], "make": [], "man": [], "mesg": [], "mkdir": [], "mkfifo": [], "more": [], "mv": [], "newgrp": [], "nice": [], "nl": [], "nm": [], "nohup": [], "od": [], "paste": [], "patch": [], "pathchk": [], "pax": [], "pr": [], "printf": [], "prs": [], "ps": [], "pwd": [], "qalter": [], "qdel": [], "qhold": [], "qmove": [], "qmsg": [], "qrerun": [], "qrls": [], "qselect": [], "qsig": [], "qstat": [], "qsub": [], "read": [], "renice": [], "rm": [], "rmdel": [], "rmdir": [], "sact": [], "sccs": [], "sed": [], "sh": [], "sleep": [], "sort": [], "split": [], "strings": [], "strip": [], "stty": [], "tabs": [], "tail": [], "talk": [], "tee": [], "test": [], "time": [], "touch": [], "tput": [], "tr": [], "true": [], "tsort": [], "tty": [], "type": [], "ulimit": [], "umask": [], "unalias": [], "uname": [], "uncompress": [], "unexpand": [], "unget": [], "uniq": [], "unlink": [], "uucp": [], "uudecode": [], "uuencode": [], "uustat": [], "uux": [], "val": [], "vi": [], "wait": [], "wc": [], "what": [], "who": [], "write": [], "xargs": [], "yacc": [], "zcat": []
     ]
     
     
@@ -51,15 +36,17 @@ class TerminalViewController: UIInputViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-                
+        
+        loadPersistedData()
+        
         collectionView.dataSource = self
         collectionView.delegate = self
         
         collectionView.registerNib(CommandCell.nib(), forCellWithReuseIdentifier: commandCellIdentifier)
         
-        nextKeyboardButton.addTarget(self, action: "advanceToNextInputMode", forControlEvents: .TouchUpInside)
-        
-        recentCommandButton.setTitle("", forState: .Normal)
+        if sentCommands.isEmpty == false {
+            recentCommandButton.setTitle(sentCommands[currentHistoryCommand], forState: .Normal)
+        }
         
         // grab key list
         for key in commands.keys {
@@ -85,15 +72,25 @@ class TerminalViewController: UIInputViewController {
             argKeys = commands[title!]!
             if argKeys.count > 0 {
                 setState(TYPE_STATE_FLAG)
+            } else {
+                argKeys = fullArgs
+                setState(TYPE_STATE_FLAG)
             }
         }
     }
     
+    // when the delete button is pressed
     @IBAction func deleteAction(sender: AnyObject) {
-        for var i = 0; i < stringLength(lastAddedCommand); i++ {
+        
+        if stringLength(lastAddedCommand) > 0 {
+            for var i = 0; i < stringLength(lastAddedCommand); i++ {
+                self.textDocumentProxy.deleteBackward()
+            }
+        } else {
             self.textDocumentProxy.deleteBackward()
         }
     }
+    
     
     var sentCommands = [String]()
     var currentHistoryCommand = 0
@@ -103,10 +100,10 @@ class TerminalViewController: UIInputViewController {
         let afterText = proxy.documentContextAfterInput
         print("before text = \(beforeText) after text = \(afterText)")
         
+        // the send button was pressed, save command
         if afterText == nil {
             if let beforeText = beforeText {
                 sentCommands.append(beforeText)
-                print("sent commands \(sentCommands)")
                 recentCommandButton.setTitle(beforeText, forState: .Normal)
                 currentHistoryCommand += 1
                 
@@ -115,16 +112,16 @@ class TerminalViewController: UIInputViewController {
         }
     }
     
+    // left arrow has been pressed
     @IBAction func setPreviousCommandAction(sender: AnyObject) {
-        print("prev")
         if currentHistoryCommand > 0 {
             currentHistoryCommand -= 1
             recentCommandButton.setTitle(sentCommands[currentHistoryCommand], forState: .Normal)
         }
     }
     
+    // right arrow has been pressed
     @IBAction func setNextCommandAction(sender: AnyObject) {
-        print("next")
         if currentHistoryCommand < sentCommands.count - 1 {
             currentHistoryCommand += 1
             recentCommandButton.setTitle(sentCommands[currentHistoryCommand], forState: .Normal)
@@ -146,6 +143,35 @@ class TerminalViewController: UIInputViewController {
         setState(TYPE_STATE_FLAG)
     }
     
+    
+    @IBAction func resignKeyboardAction(sender: AnyObject) {
+        saveData()
+        
+        advanceToNextInputMode()
+    }
+
+    // load data
+    func loadPersistedData() {
+        let prefs = NSUserDefaults.standardUserDefaults()
+        if let _sentCommands = prefs.stringArrayForKey("sentCommands") {
+            self.sentCommands = _sentCommands
+        }
+        
+        currentHistoryCommand = prefs.integerForKey("currentHistoryCommand")
+        setState(prefs.integerForKey("typeState"))
+        lastAddedCommand = prefs.valueForKey("lastAddedCommand") as! String
+    }
+    
+    // save data before closing the keyboard
+    func saveData() {
+        let prefs = NSUserDefaults.standardUserDefaults()
+        
+        prefs.setValue(sentCommands, forKey: "sentCommands")
+        prefs.setInteger(currentHistoryCommand, forKey: "currentHistoryCommand")
+        
+        prefs.setInteger(typeState, forKey: "typeState")
+        prefs.setValue(lastAddedCommand, forKey: "lastAddedCommand")
+    }
 
     // helper functions
     func stringLength(string: String) -> Int {
